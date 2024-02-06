1717728e685e422f8200abbb5108f1a2aa148d1c
libeako <libeako@earth.milky-way>
2024-02-06 22:03:42 +0100

    small changes

--- Haskell/Fana/Data/Tree/ChildrenWithInfo.hs
+++ Haskell/Fana/Data/Tree/ChildrenWithInfo.hs
@@ -4,24 +4,19 @@
 
 module Fana.Data.Tree.ChildrenWithInfo
 (
-	Forest (..), Tree (..),
-	additional_in_Forest, trees_in_Forest, trunk_in_Tree, forest_in_Tree,
+	List (..), Forest, Tree (..),
+	trunk_in_Tree, subforest_in_Tree,
 )
 where
-
 
 import Fana.Prelude
 
 import qualified Fana.Optic.Concrete.Categories.Lens as Optic
 
-data Forest a e = 
-	Forest 
-	{
-		additional :: a,
-		trees :: [Tree a e]
-	}
-	deriving (Eq, Functor, Foldable, Traversable)
 
+type List a e = (a, [e])
+
+type Forest a e = List a (Tree a e)
 
 data Tree a e =
 	Node
@@ -32,16 +27,12 @@ data Tree a e =
 	deriving (Eq, Functor, Foldable, Traversable)
 
 
+
 {- --------------------- optics ------------------------- -}
 
-additional_in_Forest :: Optic.Lens' a (Forest a e)
-additional_in_Forest = Optic.lens_from_get_set additional (\ a f -> f { additional = a })
-
-trees_in_Forest :: Optic.Lens [Tree a e1] [Tree a e2] (Forest a e1) (Forest a e2)
-trees_in_Forest = Optic.lens_from_get_set trees (\ t f -> f { trees = t })
 
 trunk_in_Tree :: Optic.Lens' e (Tree a e)
 trunk_in_Tree = Optic.lens_from_get_set trunk (\ u e -> e { trunk = u })
 
-forest_in_Tree :: Optic.Lens' (Forest a e) (Tree a e)
-forest_in_Tree = Optic.lens_from_get_set forest (\ f t -> t { forest = f })
+subforest_in_Tree :: Optic.Lens' (Forest a e) (Tree a e)
+subforest_in_Tree = Optic.lens_from_get_set forest (\ f t -> t { forest = f })
