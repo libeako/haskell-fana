adf6f3f2ee663c8888bbd61d6724264fa1a236fe
libeako <libeako@earth.milky-way>
2024-02-10 08:49:33 +0100

    Tree.ChildrenWithInfo: add traversal on additional info

--- Haskell/Fana/Data/Tree/ChildrenWithInfo.hs
+++ Haskell/Fana/Data/Tree/ChildrenWithInfo.hs
@@ -5,14 +5,15 @@
 module Fana.Data.Tree.ChildrenWithInfo
 (
 	List (..), Forest, Tree (..),
-	trunk_in_Tree, subforest_in_Tree,
+	trunk_in_Tree, subforest_in_Tree, 
+	additionals_in_Tree, additionals_in_Forest,
 )
 where
 
 import Fana.Prelude
 
 import qualified Fana.Optic.Concrete.Categories.Lens as Optic
-
+import qualified Fana.Optic.Concrete.Categories.Traversal as Optic
 
 type List a e = (a, [e])
 
@@ -36,3 +37,16 @@ trunk_in_Tree = Optic.lens_from_get_set trunk (\ u e -> e { trunk = u })
 
 subforest_in_Tree :: Optic.Lens' (Forest a e) (Tree a e)
 subforest_in_Tree = Optic.lens_from_get_set forest (\ f t -> t { forest = f })
+
+
+traverse_additionals_in_Forest :: Applicative f => (a1 -> f a2) -> Forest a1 e -> f (Forest a2 e)
+traverse_additionals_in_Forest fa (a, c) = liftA2 (,) (fa a) (traverse (traverse_additionals_in_Tree fa) c)
+
+traverse_additionals_in_Tree :: Applicative f => (a1 -> f a2) -> Tree a1 e -> f (Tree a2 e)
+traverse_additionals_in_Tree fa (Node t sf) = map (Node t) (traverse_additionals_in_Forest fa sf)
+
+additionals_in_Forest :: forall a1 a2 e . Optic.Traversal a1 a2 (Forest a1 e) (Forest a2 e)
+additionals_in_Forest = Optic.Traversal traverse_additionals_in_Forest
+
+additionals_in_Tree :: forall a1 a2 e . Optic.Traversal a1 a2 (Tree a1 e) (Tree a2 e)
+additionals_in_Tree = Optic.Traversal traverse_additionals_in_Tree
